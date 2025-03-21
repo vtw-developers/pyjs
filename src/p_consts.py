87acfd4f912bd1d4c9a4fb5f2e3e5db7d975ee76
Satbek Abdyldayev <satbekmyrza@gmail.com>
2025-03-21 13:42:16 +0900

    add some utility functions

--- src/p_consts.py
+++ src/p_consts.py
@@ -16,6 +16,7 @@ BENCHMARKS_DIR = ROOT_DIR / 'benchmarks'
 TRANSLATION_RULES_DIR = ROOT_DIR / 'translation-rules'
 TREE_SITTER_GRAMMARS_DIR = ROOT_DIR / 'tree-sitter-util'
 BUILD_DIR = ROOT_DIR / 'build'
+TMP_DIR = ROOT_DIR / 'tmp'
 
 PIREL_LOGS_DIR = LOGS_DIR / 'pirel'
 DUOGLOT_LOGS_DIR = LOGS_DIR / 'duoglot'
