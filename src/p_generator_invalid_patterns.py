e8c100792fe2704b56769be3c5978395c3de5ff9
Satbek Abdyldayev <satbekmyrza@gmail.com>
2025-05-13 13:12:22 +0900

    test TSP gen: update tests: check `float` argument_list

--- src/p_generator_invalid_patterns.py
+++ src/p_generator_invalid_patterns.py
@@ -74,3 +74,35 @@ def pattern_3_has_block_with_ret_stat_secretfn_flag_on(node: pds.DuoGlotNode, te
   if first_child.get_ts_node_type() != 'return_statement':
     return False
   return True
+
+
+def pattern_4_has_floatfn_with_empty_argument_list(node: pds.DuoGlotNode) -> bool:
+  '''
+  RETURN True if the snippet contains a float function with an empty argument list.
+  '''
+  # node must be non-terminal
+  if node.is_terminal():
+    return False
+  # node must be call
+  if node.get_ts_node_type() != 'call':
+    return False
+  # assert first child must be non-terminal
+  first_child = node.get_children()[0]
+  assert first_child.is_nonterminal(), 'first child must be non-terminal'
+  # first child must be an identifier
+  if first_child.get_ts_node_type() != 'identifier':
+    return False
+  # function name must be float
+  fn_name = first_child.get_children()[0].node_type
+  if fn_name != 'float':
+    return False
+  # assert second child must be non-terminal
+  second_child = node.get_children()[1]
+  assert second_child.is_nonterminal(), 'second child must be non-terminal'
+  # second child must be an argument list
+  if second_child.get_ts_node_type() != 'argument_list':
+    return False
+  # argument_list must have zero non-terminal children
+  if second_child.get_num_nt_children() > 0:
+    return False
+  return True
