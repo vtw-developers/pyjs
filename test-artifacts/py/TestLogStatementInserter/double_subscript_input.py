12ca1fd3b605e575bb41ff00075b37c302aafc3d
Satbek Abdyldayev <satbekmyrza@gmail.com>
2025-06-16 16:25:20 +0900

    bug fix: `LogStatementInserter`: assignment to double subscript

---
+++ test-artifacts/py/TestLogStatementInserter/double_subscript_input.py
@@ -0,0 +1,3 @@
+def f_gold(n):
+    bell = [[0 for i in range(n + 1)] for j in range(n + 1)]
+    bell[0][0] = 1
---
+++ test-artifacts/py/TestLogStatementInserter/double_subscript_output.py
@@ -0,0 +1,5 @@
+def f_gold(n):
+    bell = [[0 for i in range(n + 1)] for j in range(n + 1)]
+    myexactlog(bell)
+    bell[0][0] = 1
+    myexactlog(bell)
