7a0896b4444fd8bd84f1f309f5dbee949655ce7b
Nguyễn Gia Phong <cnx@loang.net>
2025-07-14 18:08:05 +0900

    Add statements outside fun def to precontext

--- src/p_pirel.py
+++ src/p_pirel.py
@@ -91,9 +91,10 @@ def get_pre_context_global(src_main_code: str, stat_npath: List[int]) -> str:
 
     # move up the tree
     cursor_node = cursor_node.get_parent()
-    if isinstance(cursor_node, pvpy.BlockNode):
-      if isinstance(cursor_node.get_parent(), pvpy.FunctionDefinitionNode):
-        break
+    while (isinstance(cursor_node, pvpy.BlockNode)
+           and isinstance(cursor_node.get_parent(),
+                          pvpy.FunctionDefinitionNode)):
+      cursor_node = cursor_node.get_parent()
 
   # 2. replace the context node with a special identifier
   spec_id_stat = pvpy.ExpressionStatementNode.build(
