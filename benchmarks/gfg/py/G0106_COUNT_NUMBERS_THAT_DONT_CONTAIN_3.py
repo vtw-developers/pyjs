54fe340783b7d4629e0e94f2628f5887065453ee
Nguyễn Gia Phong <cnx@loang.net>
2025-08-28 15:50:10 +0900

    GFG benchmark: port to Python 3

--- benchmarks/gfg/py/G0106_COUNT_NUMBERS_THAT_DONT_CONTAIN_3.py
+++ benchmarks/gfg/py/G0106_COUNT_NUMBERS_THAT_DONT_CONTAIN_3.py
@@ -11,9 +11,9 @@ def f_gold(n):
     elif n >= 3 and n < 10:
         return n - 1
     po = 1
-    while n / po > 9:
+    while n // po > 9:
         po = po * 10
-    msd = n / po
+    msd = n // po
     if msd != 3:
         return f_gold(msd) * f_gold(po - 1) + f_gold(msd) + f_gold(n % po)
     else:
--- benchmarks/gfg/py/G0606_SMALLEST_OF_THREE_INTEGERS_WITHOUT_COMPARISON_OPERATORS_1.py
+++ benchmarks/gfg/py/G0606_SMALLEST_OF_THREE_INTEGERS_WITHOUT_COMPARISON_OPERATORS_1.py
@@ -6,8 +6,8 @@ def test():
     result = f_gold(* parameters_set)
 "-----------------"
 def f_gold(x, y, z):
-    if not (y / x):
-        return y if (not (y / z)) else z
-    return x if (not (x / z)) else z
+    if not (y // x):
+        return y if (not (y // z)) else z
+    return x if (not (x // z)) else z
 "-----------------"
 test()
