f325551b4ab6f56e94cb0ee00cd0c96019b113f6
Nguyễn Gia Phong <cnx@loang.net>
2025-09-01 11:16:06 +0900

    GFG benchmark: make halting conditions explicit

--- benchmarks/gfg/py/G0472_NTH_MULTIPLE_NUMBER_FIBONACCI_SERIES.py
+++ benchmarks/gfg/py/G0472_NTH_MULTIPLE_NUMBER_FIBONACCI_SERIES.py
@@ -8,14 +8,13 @@ def test():
 def f_gold(k, n):
     f1 = 0
     f2 = 1
+    f3 = f1 + f2
     i = 2
-    while i != 0:
-        f3 = f1 + f2
+    while f3 % k != 0:
         f1 = f2
         f2 = f3
-        if f2 % k == 0:
-            return n * i
+        f3 = f1 + f2
         i += 1
-    return
+    return n * i
 "-----------------"
 test()
