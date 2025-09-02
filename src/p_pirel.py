9a64f2685dea34353fac3e0d27db651ff9c83eca
Satbek Abdyldayev <satbekmyrza@gmail.com>
2025-09-02 19:04:11 +0900

    do not replace function invocations in stat node, expr validation

--- src/p_pirel.py
+++ src/p_pirel.py
@@ -668,8 +668,8 @@ def _get_stat_node_val_src_program(
   to avoid infinite recursion or type errors,
   e.g. `def f_gold(r, l, arr, x):` and invocation `f_gold(arr, l, mid - 1, x)`
   '''
-  defined_fns = pvpy.DefinedFunctionNameExtractor.get_defined_function_names(snv_src_main_code)
-  snv_src_main_code = pvpy.FunctionInvocationReplacer.replace_function_invocations(snv_src_main_code, defined_fns)
+  # defined_fns = pvpy.DefinedFunctionNameExtractor.get_defined_function_names(snv_src_main_code)
+  # snv_src_main_code = pvpy.FunctionInvocationReplacer.replace_function_invocations(snv_src_main_code, defined_fns)
 
   '''
   Test call code is a simple hard-coded `test()` string
