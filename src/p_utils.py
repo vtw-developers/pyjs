vtw-developers/pyjs

commit 6b6416f4ed80420db0c140129f7100230b57540a

support loggers with different output paths

src/p_utils.py
@@ -35,7 +35,7 @@ class MultilineLogFormatter(logging.Formatter):
 
 
 # LOGGING
-def setup_logger(name: str) -> logging.Logger:
+def setup_logger(name: str, path: Optional[Path] = None) -> logging.Logger:
   '''
   Create and return a logger with a file handler (DEBUG and above)
   and a console handler (ERROR and above).
@@ -43,7 +43,7 @@ def setup_logger(name: str) -> logging.Logger:
   NOTE Logging levels:
   DEBUG, INFO, WARNING, ERROR, CRITICAL
   '''
-  _LOG_FPATH = p_consts.LOGS_DIR / 'pirel.log'
+  _LOG_FPATH = path or (p_consts.PIREL_LOGS_DIR / 'pirel.log')
   _LOG_FMODE = 'a'
   _LOG_FORMAT = '%(asctime)s,%(msecs)d %(taskName)s %(levelname)s %(module)s.%(funcName)s:%(lineno)d %(message)s'
   _LOG_FORMAT_CONSOLE = '%(asctime)s %(taskName)s %(levelname)s %(message)s'

commit 4b672847e8dcf723f6445696e28e14124cc7a1f8

fix an issue introduced by 6b6416f

src/p_utils.py
@@ -43,7 +43,7 @@ def setup_logger(name: str, path: Optional[Path] = None) -> logging.Logger:
   NOTE Logging levels:
   DEBUG, INFO, WARNING, ERROR, CRITICAL
   '''
-  _LOG_FPATH = path or (p_consts.PIREL_LOGS_DIR / 'pirel.log')
+  _LOG_FPATH = path or (p_consts.LOGS_DIR / 'pirel.log')
   _LOG_FMODE = 'a'
   _LOG_FORMAT = '%(asctime)s,%(msecs)d %(taskName)s %(levelname)s %(module)s.%(funcName)s:%(lineno)d %(message)s'
   _LOG_FORMAT_CONSOLE = '%(asctime)s %(taskName)s %(levelname)s %(message)s'