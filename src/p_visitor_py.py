vtw-developers/pyjs

commit f40e34e5724bd6345938134b07d1dcb7e47de4de

Exclude function values from trace

src/p_visitor_py.py
@@ -2786,6 +2786,9 @@ class ChoicableNodeExtractor(pvis.Visitor):
     if node.alternative:
       self.visit(node.alternative)
 
+  def visit_FunctionDefinitionNode(self, node: FunctionDefinitionNode) -> None:
+    pass
+
   def visit_IfStatementNode(self, node: IfStatementNode) -> None:
     '''
     Add condition as a choicable node only if

commit 988316862b502de0ea27aa57f720598d5150ae55

incomplete fix to a regression caused by f40e34e:
Does not allow collecting choicable nodes from functions.
In GFG, statements to be validated are placed into a function
f_gold, from which choicable nodes are collected.
Add a flag to control whether to skip function_definitions or not.
Incomplete, as does not fix the original issue f40e34e solved.

src/p_visitor_py.py
@@ -2700,12 +2700,14 @@ class ChoicableNodeExtractor(pvis.Visitor):
   def __init__(
     self,
     exclude_statement_nodes_ids: List[int] = [],
-    nid_node_map: Dict[int, pvis.AbstractNode] = {}
+    nid_node_map: Dict[int, pvis.AbstractNode] = {},
+    is_skip_fndef: bool = False,
   ):
     super().__init__()
     self.choicable_nodes : List[pvis.AbstractNode] = []
     self.exclude_statement_nodes_ids = exclude_statement_nodes_ids
     self.nid_node_map = nid_node_map
+    self.is_skip_fndef = is_skip_fndef
     self.pp = PrettyPrinter(indent_with='    ')
 
   def add_choicable_node(self, node: pvis.AbstractNode) -> None:
@@ -2787,7 +2789,17 @@ class ChoicableNodeExtractor(pvis.Visitor):
       self.visit(node.alternative)
 
   def visit_FunctionDefinitionNode(self, node: FunctionDefinitionNode) -> None:
-    pass
+    '''
+    Visit the body of the function definition only if
+    is_skip_fndef is False.
+    '''
+    if self.is_skip_fndef:
+      logger.debug(
+        f'ChoicableNodeExtractor: skipping function definition node: '
+        f'"{node.name.val()}"')
+      return
+    for child in node.get_nt_children():
+      self.visit(child)
 
   def visit_IfStatementNode(self, node: IfStatementNode) -> None:
     '''
@@ -2854,6 +2866,7 @@ class ChoicableNodeExtractor(pvis.Visitor):
     cls,
     src_main_code: str,
     exclude_statement_nodes_ids: List[int] = [],
+    is_skip_fndef: bool = False,
   ) -> List[pvis.AbstractNode]:
     '''
     Extract choicable nodes from the given src_main_code.
@@ -2865,7 +2878,7 @@ class ChoicableNodeExtractor(pvis.Visitor):
     ts_tree = src_parser.parse(bytes(src_main_code, 'utf-8'))
     tree = Tree.from_ts_tree(ts_tree)
     nid_node_map = tree.root_node.get_nid_node_map()
-    extractor = cls(exclude_statement_nodes_ids, nid_node_map)
+    extractor = cls(exclude_statement_nodes_ids, nid_node_map, is_skip_fndef)
     extractor.visit(tree.root_node)
     choicable_nodes = extractor.get_choicable_nodes()
     return choicable_nodes