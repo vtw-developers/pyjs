54b82bfd5774c6640f04ad8e803cbfbad2cb1c49
Satbek Abdyldayev <satbekmyrza@gmail.com>
2025-09-20 10:44:48 +0900

    GFG benchmark: reduce tests, maintain 100% stmt coverage, fix timeout issue

--- benchmarks/gfg/py/G0481_NUMBER_NON_NEGATIVE_INTEGRAL_SOLUTIONS_B_C_N.py
+++ benchmarks/gfg/py/G0481_NUMBER_NON_NEGATIVE_INTEGRAL_SOLUTIONS_B_C_N.py
@@ -1,6 +1,6 @@
 def test():
   "--- test function ---"
-  param =[(62,),(44,),(37,),(81,),(14,),(20,),(76,),(72,),(96,),(52,)]
+  param =[(6,),(4,),(3,),(8,),(1,),(2,),(7,),(5,),(9,),(10,)]
   for i, parameters_set in enumerate(param):
     idx = i
     result = f_gold(* parameters_set)
