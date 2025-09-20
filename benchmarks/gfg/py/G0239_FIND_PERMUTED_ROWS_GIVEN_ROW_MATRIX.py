4846af3c3db238925b1caa8bb9f03b404a4f4630
Satbek Abdyldayev <satbekmyrza@gmail.com>
2025-09-20 11:21:19 +0900

    GFG benchmark: declare a variable so that it is in scope for next stat

--- benchmarks/gfg/py/G0239_FIND_PERMUTED_ROWS_GIVEN_ROW_MATRIX.py
+++ benchmarks/gfg/py/G0239_FIND_PERMUTED_ROWS_GIVEN_ROW_MATRIX.py
@@ -24,6 +24,7 @@ def f_gold(mat, m, n, r):
     for i in range(m):
         if i == r:
             continue
+        j = 0
         for j in range(n):
             if mat[i][j] not in s:
                 j = j - 2
