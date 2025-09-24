18fd7fd0d86978849e8d45b0c2ddd7eda730bd3f
Satbek Abdyldayev <satbekmyrza@gmail.com>
2025-09-24 09:01:42 +0900

    GFG benchmark: help generator: remove paren. around fn in call

--- benchmarks/gfg/py/G0302_GOOGLE_CASE_GIVEN_SENTENCE.py
+++ benchmarks/gfg/py/G0302_GOOGLE_CASE_GIVEN_SENTENCE.py
@@ -12,10 +12,10 @@ def f_gold(s):
     i = 1
     while i < n:
         if s[i] == " " and i <= n:
-            s1 = s1 + " " + (s[i + 1]).lower()
+            s1 = s1 + " " + s[i + 1].lower()
             i = i + 1
         else:
-            s1 = s1 + (s[i]).upper()
+            s1 = s1 + s[i].upper()
         i = i + 1
     return s1
 "-----------------"
