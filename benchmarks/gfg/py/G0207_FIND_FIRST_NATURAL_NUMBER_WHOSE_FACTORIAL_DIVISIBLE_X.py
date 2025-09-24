ab9e92bff0ff267d14883c6065412165e7536685
Satbek Abdyldayev <satbekmyrza@gmail.com>
2025-09-24 16:56:59 +0900

    GFG benchmark: update scoping

--- benchmarks/gfg/py/G0207_FIND_FIRST_NATURAL_NUMBER_WHOSE_FACTORIAL_DIVISIBLE_X.py
+++ benchmarks/gfg/py/G0207_FIND_FIRST_NATURAL_NUMBER_WHOSE_FACTORIAL_DIVISIBLE_X.py
@@ -6,12 +6,13 @@ def test():
     result = f_gold(* parameters_set)
 "-----------------"
 def f_gold(x):
-    i = 1
+    k = 1
     fact = 1
     for i in range(1, x):
         fact = fact * i
         if fact % x == 0:
             break
-    return i
+        k = i
+    return k
 "-----------------"
 test()
--- benchmarks/gfg/py/G0239_FIND_PERMUTED_ROWS_GIVEN_ROW_MATRIX.py
+++ benchmarks/gfg/py/G0239_FIND_PERMUTED_ROWS_GIVEN_ROW_MATRIX.py
@@ -24,12 +24,13 @@ def f_gold(mat, m, n, r):
     for i in range(m):
         if i == r:
             continue
-        j = 0
+        k = 0
         for j in range(n):
             if mat[i][j] not in s:
                 j = j - 2
                 break
-        if j + 1 != n:
+            k = j
+        if k + 1 != n:
             continue
         print(i)
 "-----------------"
