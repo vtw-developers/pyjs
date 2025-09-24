eaa20f88b82f27c01181cab0f25713b75adf03c0
Satbek Abdyldayev <satbekmyrza@gmail.com>
2025-09-24 16:50:52 +0900

    GFG benchmark: reduce tests, maintain 100% stat coverage, prevent overflow on *

--- benchmarks/gfg/py/G0207_FIND_FIRST_NATURAL_NUMBER_WHOSE_FACTORIAL_DIVISIBLE_X.py
+++ benchmarks/gfg/py/G0207_FIND_FIRST_NATURAL_NUMBER_WHOSE_FACTORIAL_DIVISIBLE_X.py
@@ -1,6 +1,6 @@
 def test():
   "--- test function ---"
-  param =[(67,),(47,),(57,),(89,),(67,),(40,),(16,),(83,),(93,),(43,)]
+  param =[(6,),(4,),(5,),(9,),(7,),(2,),(1,),(3,),(8,),(10,)]
   for i, parameters_set in enumerate(param):
     idx = i
     result = f_gold(* parameters_set)
