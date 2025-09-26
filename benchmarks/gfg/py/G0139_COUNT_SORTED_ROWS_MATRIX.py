df7a67b90e1e94f38367ef28572de89df2b37158
Satbek Abdyldayev <satbekmyrza@gmail.com>
2025-09-27 00:34:16 +0900

    GFG benchmark: update scoping

--- benchmarks/gfg/py/G0139_COUNT_SORTED_ROWS_MATRIX.py
+++ benchmarks/gfg/py/G0139_COUNT_SORTED_ROWS_MATRIX.py
@@ -27,11 +27,12 @@ def f_gold(mat, r, c):
         if j == c - 2:
             result += 1
     for i in range(0, r):
-        j = 0
+        k = 0
         for j in range(c - 1, 0, -1):
             if mat[i][j - 1] <= mat[i][j]:
                 break
-        if c > 1 and j == 1:
+            k = j
+        if c > 1 and k == 1:
             result += 1
     return result
 "-----------------"
