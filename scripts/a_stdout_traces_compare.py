6097c925b5ec1479b259fbf23a955f9e3ef93581
Satbek Abdyldayev <satbekmyrza@gmail.com>
2025-09-27 03:55:16 +0900

    update scripts/a_stdout_traces_compare.py

--- scripts/a_stdout_traces_compare.py
+++ scripts/a_stdout_traces_compare.py
@@ -16,3 +16,9 @@ tar_trace = p_code_runner._extract_trace_from_stdout(tar_stdout)
 
 are_traces_identical = p_rule_applicator.are_traces_equal_rec(src_trace, tar_trace)
 print(f'src_trace == tar_trace: {are_traces_identical}')
+
+if not are_traces_identical:
+  trace_mismatch_idx = p_rule_applicator._get_trace_mismatch_idx(src_trace, tar_trace)
+  print(f'trace_mismatch_idx: {trace_mismatch_idx}')
+  print(f'src_trace: {src_trace[2][trace_mismatch_idx]}')
+  print(f'tar_trace: {tar_trace[2][trace_mismatch_idx]}')
