113a9ec61932106124f9b3328673f1287e5e699d
Satbek Abdyldayev <satbekmyrza@gmail.com>
2025-09-28 18:25:26 +0900

    GFG benchmark: reduce tests, maintain 100% stat cov, prevent timeouts

--- benchmarks/gfg/py/G0140_COUNT_STRINGS_ADJACENT_CHARACTERS_DIFFERENCE_ONE.py
+++ benchmarks/gfg/py/G0140_COUNT_STRINGS_ADJACENT_CHARACTERS_DIFFERENCE_ONE.py
@@ -1,6 +1,6 @@
 def test():
   "--- test function ---"
-  param =[(7,),(47,),(72,),(66,),(71,),(56,),(61,),(68,),(78,),(22,)]
+  param =[(2,),(3,),(4,),(5,),(6,),(7,),(8,),(9,),(10,),(11,)]
   for i, parameters_set in enumerate(param):
     idx = i
     result = f_gold(* parameters_set)
