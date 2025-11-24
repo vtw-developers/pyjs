27a15d4cb190adeb8cb46d5941fd7e584ea64152
Satbek Abdyldayev <satbekmyrza@gmail.com>
2025-11-24 14:09:40 +0900

    edit src/p_consts.py: use "mini-benchmark"

--- src/p_consts.py
+++ src/p_consts.py
@@ -270,12 +270,13 @@ TEST_MAIN_CALL_DELIMITER = '"-----------------"'
 STARTING_RULESET_FPATH = TRANSLATION_RULES_DIR / 'starting-ruleset.snart'
 
 GFG_BENCHMARK_DIR = BENCHMARKS_DIR / 'gfg' / 'py'
+MINI_GFG_BENCHMARK_DIR = BENCHMARKS_DIR / 'mini-benchmark'
 GFG_TRULES_MAIN_FPATH = TRANSLATION_RULES_DIR / 'main' / 'gfg.snart'
 GFG_TRULES_TEST_FPATH = TRANSLATION_RULES_DIR / 'test' / 'gfg.snart'
 
 BENCHMARK_CONFIGS = {
   'gfg': {
-    'benchmark_dir': GFG_BENCHMARK_DIR,
+    'benchmark_dir': MINI_GFG_BENCHMARK_DIR,
     'translation_rules_main_code_fpath': GFG_TRULES_MAIN_FPATH,
     'translation_rules_test_code_fpath': GFG_TRULES_TEST_FPATH,
   },
